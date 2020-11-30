b7f2ce1f3db80608d7163bafe35eb1cec1827378
subaojing <subaojing@kinfsoft.com>
2020-11-30 13:51:55 +0800

    多样式的文字

--- lib/Demo/BasicDemo.dart
+++ lib/Demo/BasicDemo.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class BasicDemo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  TextDemo();
+    // return
+
+  }
+}
+
+class TextDemo extends StatelessWidget {
   final TextStyle _textStyle = TextStyle(
     fontSize: 16.0,
   );
@@ -10,11 +21,38 @@ class BasicDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Text(
-        '《 $_author 》-- $_title 。HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
-        textAlign: TextAlign.left,
-      style: _textStyle,
-      maxLines: 3,
-      overflow: TextOverflow.ellipsis,
+            '《 $_author 》-- $_title 。HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
+            textAlign: TextAlign.left,
+          style: _textStyle,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+        );
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RichText(
+        text: TextSpan(
+            text: 'susu',
+            style: TextStyle(
+              color: Colors.deepPurple,
+              fontSize: 34.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w100,
+            ),
+            children: [
+              TextSpan(
+                  text: '.net',
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    color: Colors.grey,
+                  )
+              )
+            ]
+        )
     );
   }
 }
