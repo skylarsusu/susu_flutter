4baf731535b5a9304825ad9db1a247dba6a5cd1d
subaojing <subaojing@kinfsoft.com>
2020-11-30 15:32:36 +0800

    Row和Column排列小部件

--- lib/Demo/layout_demo.dart
+++ lib/Demo/layout_demo.dart
@@ -4,7 +4,20 @@ class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return IconBadge(Icons.pool);
+    // return IconBadge(Icons.pool);
+    // return Column(
+    //   children: [
+    //     IconBadge(Icons.pool,size: 32.0,)
+    //   ],
+    return Container(
+      child: Column(//Row
+        children: [
+          IconBadge(Icons.pool),
+          IconBadge(Icons.beach_access),
+          IconBadge(Icons.airplanemode_active),
+        ],
+      ),
+    );
   }
 }
 class IconBadge extends StatelessWidget {
