1c62d7e764366d772396a788d040408df1abd90e
subaojing <subaojing@kinfsoft.com>
2020-11-30 14:06:02 +0800

    添加装饰盒子外边框

--- lib/Demo/BasicDemo.dart
+++ lib/Demo/BasicDemo.dart
@@ -8,14 +8,25 @@ class BasicDemo extends StatelessWidget {
     return  Container(
       color: Colors.grey[100],
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             child: Icon(Icons.pool, size: 32.0,color: Colors.white),
-            color: Color.fromRGBO(255, 54, 255, 1.0),
+            // color: Color.fromRGBO(255, 54, 255, 1.0),
             padding: EdgeInsets.only(left: 24.0, right: 8.0),
             margin: EdgeInsets.all(8.0),
             width: 90.0,
             height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(255, 54, 255, 1.0),
+              border: Border(
+                top: BorderSide(
+                  color: Colors.indigoAccent[100],
+                  width: 3.0,
+                  style: BorderStyle.solid,
+                )
+              )
+            ),
           )
         ],
       ),
