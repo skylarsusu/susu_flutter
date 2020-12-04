af441cab7fb6d67a36ade750dc781992c98780e0
subaojing <subaojing@kinfsoft.com>
2020-12-04 12:31:10 +0800

    ReplaySubject

--- lib/Demo/rxdart/rxdart_demo.dart
+++ lib/Demo/rxdart/rxdart_demo.dart
@@ -28,17 +28,22 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     super.initState();
 
     // PublishSubject<String> _subject = PublishSubject<String>();
-    BehaviorSubject<String> _subject = BehaviorSubject<String>();
+    // BehaviorSubject<String> _subject = BehaviorSubject<String>();//两个消息都可以听到，若两个消息挨着会听到后面的消息
+    ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);//监听器可以听到所有的消息,maxSize: 2控制监听的最大信息量，此为后两个
 
+    _subject.add('hello');
+    _subject.add('hola');
+    _subject.add('hi');
     _subject.listen((value) {
       print('listen 1 : $value');
     });
-    _subject.add('hello');
+    // _subject.add('hello');
 
     _subject.listen((value) {
       print('listen 2 : $value');
     });
-    _subject.add('hola');
+    // _subject.add('hola');
+
     // Observable<String> _observable = Observable(Stream);
     // Observable<String> _observable;
 
