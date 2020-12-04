551980f013486ddecf82480c9e3c3a5123188dac
subaojing <subaojing@kinfsoft.com>
2020-12-04 10:51:25 +0800

    StreamBuilder

--- lib/Demo/stream/stream_demo.dart
+++ lib/Demo/stream/stream_demo.dart
@@ -108,7 +108,14 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(_data),
+            // Text(_data),
+            StreamBuilder(
+              stream: _streamDemo.stream,
+                initialData: '...',
+                builder: (context, snapshot) {
+                  return Text('${snapshot.data}');
+                },
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
