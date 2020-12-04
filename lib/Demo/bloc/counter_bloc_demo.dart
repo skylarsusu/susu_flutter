6225083ee28bedebecaae891fa66efc117c40bb8
subaojing <subaojing@kinfsoft.com>
2020-12-04 15:05:06 +0800

    Bloc + Stream输出数据

--- lib/Demo/bloc/counter_bloc_demo.dart
+++ lib/Demo/bloc/counter_bloc_demo.dart
@@ -1,17 +1,25 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-
 
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return Center(
-      child: ActionChip(
-        label: Text('0'),
-        onPressed: (){
-          _counterBloc.log();
+      child: StreamBuilder(
+        initialData: 0,
+        stream: _counterBloc.count,
+        builder: (context, snapshot) {
+          return ActionChip(
+            label: Text('${snapshot.data}'),
+            onPressed: (){
+              // _counterBloc.log();
+              _counterBloc.counter.add(1);
+            },
+          );
         },
-      ),
+
+      )
     );
   }
 }
@@ -22,7 +30,8 @@ class CounterActionButton extends StatelessWidget {
     CounterBloc _counterBloc = CounterProvider.of(context).bloc;
     return FloatingActionButton(
         onPressed: () {
-          _counterBloc.log();
+          // _counterBloc.log();
+          _counterBloc.counter.add(1);
         },
       child: Icon(Icons.add),
     );
@@ -49,7 +58,31 @@ class CounterProvider extends InheritedWidget {
 }
 
 class CounterBloc {
+  int _count = 0;
+
+  final _counterActionController = StreamController<int>();
+  StreamSink<int> get counter => _counterActionController.sink;
+
+  final _counterController = StreamController<int>();
+  Stream<int> get count => _counterController.stream;
+  CounterBloc() {
+    _counterActionController.stream.listen((event) {
+      print('event: $event');
+      _count = event + _count;
+      _counterController.add(_count);
+    });
+
+  }
+
+
+  void disponse() {
+      _counterActionController.close();
+      _counterController.close();
+  }
+
+
   void log() {
     print('Bloc');
+
   }
 }
